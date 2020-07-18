22b4d3aa6892105201c469da459f94e9d705d3b9
Lozhka <diabloakk57@gmail.com>
2020-07-18 15:51:08 +0300

    Delete Lesson_1_task_3.py

--- Lesson_1_task_3.py
+++
@@ -1,6 +0,0 @@
-# Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
-# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
-
-number = int(input('Введите число: '))
-result = number + int(str(number)*2) + int(str(number)*3)
-print(result)
