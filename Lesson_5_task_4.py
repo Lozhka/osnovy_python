fc5a313345aa65a4b348e2a33291cbdbf5bd2f96
Lozhka <diabloakk57@gmail.com>
2020-07-28 13:05:41 +0300

    Update Lesson_5_task_4.py

---
+++ Lesson_5_task_4.py
@@ -0,0 +1,16 @@
+# Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
+
+with open('task_4.txt', encoding = 'utf8') as f:
+    lines = f.readlines()
+
+with open('task_44.txt', 'w', encoding = 'utf8') as f:
+    for line in lines:
+        if '1' in line:
+            line = line.replace('One', 'Один')
+        elif '2' in line:
+            line = line.replace('Two', 'Два')
+        elif '3' in line:
+            line = line.replace('Three', 'Три')
+        elif '4' in line:
+            line = line.replace('Four', 'Четыре')
+        f.write(line)
