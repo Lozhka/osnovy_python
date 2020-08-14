da20788da715598c995c962856bb8e489b3bab4c
Lozhka <diabloakk57@gmail.com>
2020-08-14 19:12:03 +0300

    Update Lesson_8_task_3.py

---
+++ Lesson_8_task_3.py
@@ -0,0 +1,33 @@
+"""
+Создайте собственный класс-исключение, который должен проверять содержимое списка на наличие только чисел.
+Проверить работу исключения на реальном примере.
+Необходимо запрашивать у пользователя данные и заполнять список. Класс-исключение должен контролировать типы данных элементов списка.
+"""
+
+import re
+
+
+class OwnError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+my_list = []
+my_numbers = ""
+
+while my_numbers != "q":
+    my_numbers = input("Введите число: ")
+    if my_numbers == "q":
+        print("Программа завершена.")
+        break
+    numbers = my_numbers.split(" ")
+    try:
+        for i in numbers:
+            for j in i:
+                j = j.isalpha()
+                if j:
+                    raise OwnError("Вы ввели не число.")
+    except OwnError as o_e:
+        print(o_e)
+    finally:
+        my_list += re.findall(r'\d+', my_numbers)
+        print(" ".join(my_list))
